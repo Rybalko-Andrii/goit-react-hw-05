18402f7db556dc0465e65b7db9469e7d6828e5ab
Rybalko Andrii <0andryha0@gmail.com>
2025-04-18 14:32:00 +0200

    add API

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { getTrandMovies } from "./components/Api";
+
+function App() {
+  getTrandMovies();
+  console.log(getTrandMovies());
+
+  return <></>;
+}
+
+export default App;
---
+++ src/components/Api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const API_KEY = "otlkal3vSHjIR6JX_6DdywDqgynWnyfsOiMAhCs4sAE";
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZGY5ZGIzOWRjMzJiOTE1ZWU1NDE5ODMwOTRmMmRiNyIsIm5iZiI6MTc0NDk2NzU0MS4wMzMsInN1YiI6IjY4MDIxNzc1ZDMxN2JlNWU1Yzk5MjQyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hP8qrS3giAN-TUTj8_u5hL9UEAX4n1ZnmCLWg9oOD-I";
+
+const getMovies = async (endpoint, params = {}) => {
+  const url = `https://api.themoviedb.org/3${endpoint}`;
+
+  const options = {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    params: { language: "en-US", ...params },
+  };
+
+  try {
+    const response = await axios.get(url, options);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const getTrandMovies = (page) => {
+  return getMovies("/trending/movie/day", { page });
+};
+
+export const getMoviesByKeyWords = (query, page = 1) => {
+  return getMovies("/search/movie", { query, page });
+};
+
+export const getMovieById = (movieId) => {
+  return getMovies(`/movie/${movieId}`);
+};
+
+export const gethMovieCastById = (movieId) => {
+  return getMovies(`/movie/${movieId}/credits`);
+};
+
+export const getMovieReviewsById = (movieId) => {
+  return getMovies(`/movie/${movieId}/reviews`);
+};
